e001c3be21b504ded926599cce0ffb40f3c8b32c
aki <lala.akira@gmail.com>
2019-09-03 09:53:18 +0900

    Updated test environment to rails 6.0.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,15 @@
 require "bundler/setup"
-require "dummy/application"
-require "activestorage/validator"
+require "active_storage/engine"
+Bundler.require :default, :development
 require "support/blob_helper"
+
+Combustion.initialize! :active_record, :active_job do
+  if ActiveRecord::VERSION::MAJOR < 6 && config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
+    config.active_record.sqlite3.represent_boolean_as_integer = true
+  end
+  config.active_job.queue_adapter = :inline
+  config.active_storage.service = :test
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
