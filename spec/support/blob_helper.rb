44b6435d5bece769e1d8f355463b617bd16e7b7a
Peter Goldstein <peter.m.goldstein@gmail.com>
2022-03-14 09:48:42 -0700

    Add Ruby 3.1 and Rails 7 to the CI matrix

--- spec/support/blob_helper.rb
+++ spec/support/blob_helper.rb
@@ -1,6 +1,10 @@
 module BlobHelper
   def create_file_blob(filename:, content_type: "image/jpeg", metadata: nil)
-    ActiveStorage::Blob.create_after_upload! io: file_fixture(filename).open, filename: filename, content_type: content_type, metadata: metadata
+    if Rails::VERSION::MAJOR < 6 || (Rails::VERSION::MAJOR == 6 && Rails::VERSION::MINOR == 0) 
+      ActiveStorage::Blob.create_after_upload! io: file_fixture(filename).open, filename: filename, content_type: content_type, metadata: metadata
+    else
+      ActiveStorage::Blob.create_and_upload! io: file_fixture(filename).open, filename: filename, content_type: content_type, metadata: metadata
+    end
   end
 
   private
