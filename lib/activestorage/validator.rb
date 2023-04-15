3baac369b6602010d49f5efaad4bc11041a2a19a
aki <lala.akira@gmail.com>
2023-04-15 12:10:21 +0900

    refactor: Remove unused monkey patch

--- lib/activestorage/validator.rb
+++ lib/activestorage/validator.rb
@@ -4,15 +4,4 @@ ActiveSupport.on_load(:active_record) do
   require 'activestorage/validator/blob'
 end
 
-module ActiveStorage
-  # https://github.com/rails/rails/commit/42259ce904eb2538761b32a793cbe390fb8272b7
-  if Rails::VERSION::MAJOR < 6
-    class Attached::One < Attached
-      def blank?
-        attachment.blank?
-      end
-    end
-  end
-end
-
 I18n.load_path += Dir[File.expand_path(File.join(__dir__, '../../config/locales', '*.yml')).to_s]
